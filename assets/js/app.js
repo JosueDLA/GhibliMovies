fb3c05f13878ca2348e7302c252d3b4708693348
JosueDLA <josuedlavs@gmail.com>
2020-08-23 06:42:07 -0600

    Fixed Auto Load

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-document.querySelector('#movies').addEventListener('click', function () {
+window.onload = function () {
     getData();
-});
+};
 
 function getData() {
     console.log('Movies!');
